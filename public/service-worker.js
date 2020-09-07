bb40e6f3753caa768ce7e7b7a393638e1c69bc91
William Fullerton <wrk.fullerton@gmail.com>
2020-09-07 17:03:23 -0500

    changes made to service-worker.js

--- public/service-worker.js
+++ public/service-worker.js
@@ -4,55 +4,46 @@ const DATA_CACHE_NAME = "data-cache-v1";
 
 const FILES_TO_CACHE = [
     "/",
+    "index.html",
+    "/styles.css",
     "/icons/icon-192x192.png",
     "/icons/icon-512x512.png",
-    "/index.html",
-    "/index.js",
-    "/db.js",
-    "/manifest.json",
-    "/styles.css",
-    "https://stackpath.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css",
-  "https://cdn.jsdelivr.net/npm/chart.js@2.8.0"
-];
+    "index.js",
+    "db.js"
+]
 
+const PRECACHE = 'precache-v1';
+const RUNTIME = 'runtime';
 
 self.addEventListener("install", (event) => {
     event.waitUntil(
-        caches.open(CACHE_NAME).then(function(cache) {
-            console.log("Opened cache");
-            return cache.addAll(FILES_TO_CACHE);
-        })
+        caches.open(PRECACHE)
+        .then(cache => cache.addAll(FILES_TO_CACHE))
+        .then(self.skipWaiting())
     );
 });
 
-self.addEventListener("install", function(event) {
-    if (event.request.url.includes("/api/")) {
-        event.responsdWith(
-            caches.open(DATA_CACHE_NAME).then(cache => {
-                return fetch(event.request)
-                .then(response => {
-                    if (response.status === 200) {
-                        cache.put(event.request.url, response.clone());1
-                    }
-
-                    return response;
-                })
-                .catch(err => {
-                    return cache.match(event.request);
-                });
-
-            }).catch(err => console.log(err))
+self.addEventListener("activate", event => {
+    const currentCaches = [PRECACHE, RUNTIME];
+    event.waitUntil(
+        caches.keys().then(cacheName => {
+            return cacheNames.filter(cacheName => !currentCaches.includes(cacheName));
+        }).then(cachesToDelete => {
+            return Promise.all(cachesToDelete.map(cachesToDelete => {
+                return caches.delete(cachesToDelete);
+            }));
+        }).then(() => self.clients.claim())
         );
-        return; 
-    }
+    });
 
+self.addEventListener("fetch", function (event) {
     event.respondWith(
-        fetch(event.request).catch(function() {
-            return caches.match(event.request).then(function(response) {
-                if (reponse) {
+        fetch(event.request).catch(function () {
+            return caches.match(event.request).then(function (response) {
+                if (response) {
                     return response; 
                 } else if (event.request.headers.get("accept").includes("text/html")) {
-                    return caches.match("/");
+                    return caches.match("/index.html");
                 }
             });
         })
