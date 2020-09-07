ff65059932b82c7a08c681aa1dd406c4da70f488
William Fullerton <wrk.fullerton@gmail.com>
2020-09-07 16:48:11 -0500

    changes made to index.js

--- public/index.js
+++ public/index.js
@@ -1,12 +1,3 @@
-if ('serviceWorker' in navigator) {
-  window.addEventListener('load', () => {
-    navigator.serviceWorker.register('/service-worker.js')
-    .then((reg) => {
-      console.log('Service worker has been registered.', reg);
-    });
-  });
-}
-
 let transactions = [];
 let myChart;
 
@@ -23,44 +14,44 @@ fetch("/api/transaction")
     populateChart();
   });
 
-function populateTotal() {
-  // reduce transaction amounts to a single total value
-  let total = transactions.reduce((total, t) => {
-    return total + parseInt(t.value);
-  }, 0);
+  function populateTotal() {
+    // reduce transaction amounts to a single total value
+    let total = transactions.reduce((total, t) => {
+      return total + parseInt(t.value);
+    }, 0);
+  
+    let totalEl = document.querySelector("#total");
+    totalEl.textContent = total;
+  }
 
-  let totalEl = document.querySelector("#total");
-  totalEl.textContent = total;
-}
+  function populateTable() {
+    let tbody = document.querySelector("#tbody");
+    tbody.innerHTML = "";
+  
+    transactions.forEach(transaction => {
+      // create and populate a table row
+      let tr = document.createElement("tr");
+      tr.innerHTML = `
+        <td>${transaction.name}</td>
+        <td>${transaction.value}</td>
+      `;
+  
+      tbody.appendChild(tr);
+    });
+  }
 
-function populateTable() {
-  let tbody = document.querySelector("#tbody");
-  tbody.innerHTML = "";
-
-  transactions.forEach(transaction => {
-    // create and populate a table row
-    let tr = document.createElement("tr");
-    tr.innerHTML = `
-      <td>${transaction.name}</td>
-      <td>${transaction.value}</td>
-    `;
-
-    tbody.appendChild(tr);
-  });
-}
-
-function populateChart() {
-  // copy array and reverse it
-  let reversed = transactions.slice().reverse();
-  let sum = 0;
-
-  // create date labels for chart
-  let labels = reversed.map(t => {
-    let date = new Date(t.date);
-    return `${date.getMonth() + 1}/${date.getDate()}/${date.getFullYear()}`;
-  });
-
-  // create incremental values for chart
+  function populateChart() {
+    // copy array and reverse it
+    let reversed = transactions.slice().reverse();
+    let sum = 0;
+  
+    // create date labels for chart
+    let labels = reversed.map(t => {
+      let date = new Date(t.date);
+      return `${date.getMonth() + 1}/${date.getDate()}/${date.getFullYear()}`;
+    });
+  
+      // create incremental values for chart
   let data = reversed.map(t => {
     sum += parseInt(t.value);
     return sum;
@@ -108,20 +99,21 @@ function sendTransaction(isAdding) {
     date: new Date().toISOString()
   };
 
-  // if subtracting funds, convert amount to negative number
-  if (!isAdding) {
-    transaction.value *= -1;
-  }
-
-  // add to beginning of current array of data
-  transactions.unshift(transaction);
-
-  // re-run logic to populate ui with new record
-  populateChart();
-  populateTable();
-  populateTotal();
+    // if subtracting funds, convert amount to negative number
+    if (!isAdding) {
+      transaction.value *= -1;
+    }
   
-  // also send to server
+    // add to beginning of current array of data
+    transactions.unshift(transaction);
+  
+    // re-run logic to populate ui with new record
+    populateChart();
+    populateTable();
+    populateTotal();
+    
+
+      // also send to server
   fetch("/api/transaction", {
     method: "POST",
     body: JSON.stringify(transaction),
@@ -160,3 +152,15 @@ document.querySelector("#add-btn").onclick = function() {
 document.querySelector("#sub-btn").onclick = function() {
   sendTransaction(false);
 };
+
+(function () {
+  if ('serviceWorker' in navigator) {
+    navigator.serviceWorker.register("./service-worker.js", { scope: "/"})
+    .then(() => console.log("Service Work registered successfully"))
+    .catch(error => console.log("Service worked registeration failed:", error));
+  }
+})();
+
+
+
+
